817762d922241743dece4cf749b930c4318b9856
Alexis Svinartchouk <alexis@resin.io>
2020-12-11 18:42:38 +0100

    Name images balenablocks/$ARCH-balena-electron-env:$GIT_TAG
    Change-type: patch

--- image-builder.sh
+++ image-builder.sh
@@ -8,10 +8,10 @@ function build_and_push_image () {
   echo "Building for arch name $BALENA_ARCH, platform $DOCKER_ARCH pushing to $DOCKER_REPO/$REPO_NAME"
 
   sed "s/%%BALENA_ARCH%%/$BALENA_ARCH/g" ./Dockerfile.template > ./Dockerfile.$BALENA_ARCH
-  docker buildx build -t $DOCKER_REPO/$REPO_NAME:$BALENA_ARCH --platform $DOCKER_ARCH --file Dockerfile.$BALENA_ARCH .
+  docker buildx build -t $DOCKER_REPO/$BALENA_ARCH-$REPO_NAME:$VERSION --platform $DOCKER_ARCH --file Dockerfile.$BALENA_ARCH .
 
   echo "Publishing..."
-  docker push $DOCKER_REPO/$REPO_NAME:$BALENA_ARCH
+  docker push $DOCKER_REPO/$BALENA_ARCH-$REPO_NAME:$VERSION
 
   echo "Cleaning up..."
   rm Dockerfile.$BALENA_ARCH
@@ -20,6 +20,7 @@ function build_and_push_image () {
 # You can pass in a repo (such as a test docker repo) or accept the default
 DOCKER_REPO=${1:-balenablocks}
 REPO_NAME="balena-electron-env"
+VERSION=$(git describe --tags)
 
 build_and_push_image "aarch64" "linux/arm64"
 build_and_push_image "armv7hf" "linux/arm/v7"
