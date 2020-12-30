19a94c58d982bc046a7b6e393e9d9e7ef22b59d4
Alexis Svinartchouk <alexis@resin.io>
2020-12-30 11:56:04 +0100

    Revert "Turn the screen off and on on start"
    This reverts commit 0f7862f6c2abbd5d8e4e73c16ea1de709ef3efa0.

--- src/screensaver.ts
+++ src/screensaver.ts
@@ -24,10 +24,6 @@ export async function screenOff(): Promise<void> {
 	await execFile('xset', 'dpms', 'force', 'off');
 }
 
-async function screenOn(): Promise<void> {
-	await execFile('xset', 'dpms', 'force', 'on');
-}
-
 let screensaverDisabled = false;
 
 async function setSleepDelay(value?: string): Promise<void> {
@@ -85,12 +81,6 @@ async function setScreensaverHooks(): Promise<void> {
 }
 
 export async function init(): Promise<void> {
-	// When the container is restarted when the screen was off,
-	// we need to turn the screen off and on again to make it work.
-	// screenOn() without screenOff() before has no effect.
-	// We need to do this before setScreensaverHooks().
-	await screenOff();
-	await screenOn();
 	electron.ipcMain.handle('disable-screensaver', async () => {
 		debug('disabling screensaver');
 		await setSleepDelay('never');
